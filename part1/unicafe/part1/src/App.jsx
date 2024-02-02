183d9f898523a9dc41f7e93492627871cc526212
georgina puig <georginaxpuig@gmail.com>
2024-02-02 15:24:23 -0300

    1.7: unicafe, step 2

--- part1/unicafe/part1/src/App.jsx
+++ part1/unicafe/part1/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
+import './App.css';
 
 // eslint-disable-next-line react/prop-types
-const Button = ({ text, handleClick }) => {
-  return <button onClick={handleClick}>{text}</button>;
+const Button = ({ text, handleClick, btnClass }) => {
+  return (
+    <button className={btnClass} onClick={handleClick}>
+      {text}
+    </button>
+  );
 };
 
 const App = () => {
@@ -10,18 +15,32 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good * 100) / all;
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text='good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Button
+        btnClass='good'
+        handleClick={() => setGood(good + 1)}
+        text='good'
+      />
+      <Button
+        btnClass='neutral'
+        handleClick={() => setNeutral(neutral + 1)}
+        text='neutral'
+      />
+      <Button btnClass='bad' handleClick={() => setBad(bad + 1)} text='bad' />
 
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p className='goodText'>good: {good}</p>
+      <p className='neutralText'>neutral: {neutral}</p>
+      <p className='badText'>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive}%</p>
     </div>
   );
 };
